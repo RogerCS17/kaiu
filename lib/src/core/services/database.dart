b5d08e30492f4a93331030ac788e3d2d8fcb85ab
RoyalGuard97 <51274711+RoyalGuard97@users.noreply.github.com>
2024-01-01 17:27:14 -0500

    Fix Images V3

--- lib/src/core/services/database.dart
+++ lib/src/core/services/database.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class DatabaseMethods {
+
   // Future addEmployeeDetails(Map<String, dynamic> employeeInfoMap, String id) async {
   //   //Llamamos a una Instancia de la Base de Datos
   //   return await FirebaseFirestore.instance
@@ -10,13 +11,16 @@ class DatabaseMethods {
   //       .doc(id)
   //       .set(employeeInfoMap);
   // }
+
   Stream<QuerySnapshot<Map<String, dynamic>>> getUltraDetails() {
     return FirebaseFirestore.instance.collection("Ultra").snapshots();
   }
+  
   Future<String> getImageUrl(String imagePath) async {
     Reference ref = FirebaseStorage.instance.ref().child(imagePath);
     return await ref.getDownloadURL();
   }
+
   // Future updateEmployeeDetail(String id, Map<String, dynamic> updateInfo) async{
   //   return await FirebaseFirestore.instance.collection("Employee").doc(id).update(updateInfo);
   // }
